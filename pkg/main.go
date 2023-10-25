e91046f63205684abbfa03dbd2ba868709929cae
go-ai-agent <mark.wilsonai@outlook.com>
2023-10-25 18:28:48 -0500

    Update main.go

--- pkg/main.go
+++ pkg/main.go
@@ -10,31 +10,53 @@ import (
 	"net/url"
 )
 
+const (
+	ActivityUrl   = "http://localhost:8080/go-ai-agent/example-domain/activity/entry"
+	SloUrl        = "http://localhost:8080/go-ai-agent/example-domain/slo/entry"
+	TimeseriesUrl = "http://localhost:8080/go-ai-agent/example-domain/timeseries/entry"
+
+	ActivityResource   = "file://[cwd]/pkg/resource/activity.json"
+	SloResource        = "file://[cwd]/pkg/resource/slo.json"
+	TimeseriesResource = "file://[cwd]/pkg/resource/timeseries.json"
+)
+
 func main() {
-	LoadActivity()
+	Put(ActivityResource, ActivityUrl)
+	Put(SloResource, SloUrl)
+	Put(TimeseriesResource, TimeseriesUrl)
+
+	//Delete(ActivityUrl)
+	//Delete(SloUrl)
+	//Delete(TimeseriesUrl)
 }
 
-func LoadActivity() {
-	s := "file://[cwd]/pkg/resource/activity.json"
-	u, _ := url.Parse(s)
-
+func Put(file, uri string) {
+	u, _ := url.Parse(file)
 	buf, err := httpx.ReadFile(u)
 	if err != nil {
-		fmt.Printf("err: %v", err)
+		fmt.Printf("read file err: %v\n", err)
 		return
 	}
-	//fmt.Printf("buf: %v", string(buf))
-
-	uri := "http://localhost:8080/go-ai-agent/example-domain/activity/entry"
 	reader := bytes.NewReader(buf)
 	req, err1 := http.NewRequest(http.MethodPut, uri, reader)
 	if err1 != nil {
-		fmt.Printf("err: %v", err1)
+		fmt.Printf("new request err: %v\n", err1)
+		return
 	}
-
 	resp, _ := exchange.Do[runtime.DebugError](req)
 	if resp != nil {
-		fmt.Printf("StatusCode: %v", resp.StatusCode)
+		fmt.Printf("StatusCode: %v\n", resp.StatusCode)
 	}
+}
 
+func Delete(uri string) {
+	req, err1 := http.NewRequest(http.MethodDelete, uri, nil)
+	if err1 != nil {
+		fmt.Printf("new request err: %v\n", err1)
+		return
+	}
+	resp, _ := exchange.Do[runtime.DebugError](req)
+	if resp != nil {
+		fmt.Printf("StatusCode: %v\n", resp.StatusCode)
+	}
 }
